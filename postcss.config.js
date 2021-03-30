cc313ee71cd8490a0d00a164a90c4867812f7438
Alessandro Burato <alessandro.burato@outlook.com>
2021-03-30 13:23:53 +0200

    use hugo_stats + PostProcess for perfect CSS purge https://gohugo.io/hugo-pipes/postprocess/

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,11 @@
 module.exports = {
-  map: false,
   plugins: {
     "@fullhuman/postcss-purgecss": {
-      content: ["./themes/**/*.html"],
+      content: ["./hugo_stats.json"],
+      defaultExtractor: (content) => {
+        let els = JSON.parse(content).htmlElements;
+        return [...els.tags, ...els.classes, ...els.ids, "dark"];
+      },
     },
     autoprefixer: {},
     cssnano: { preset: "default" },
